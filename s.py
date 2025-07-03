b51d817e8ea414ed7a9769fa65ccf562c35f4ec7
eyren <159797095+dicoderin@users.noreply.github.com>
2025-07-03 22:08:39 +0700

    Update s.py

--- s.py
+++ s.py
@@ -39,8 +39,8 @@ class PharosTestnet:
             "User-Agent": FakeUserAgent().random
         }
         self.BASE_API = "https://api.pharosnetwork.xyz"
-        self.RPC_URL = "https://api.zan.top/node/v1/pharos/testnet/f4a9eb274053406d91e67c193867a80a"
-        self.ALT_RPC_URL = "https://api.zan.top/node/v1/pharos/testnet/f4a9eb274053406d91e67c193867a80a"
+        self.RPC_URL = "https://testnet.dplabs-internal.com"
+        self.ALT_RPC_URL = "https://testnet.dplabs-internal.com"
         self.TOKENS = {
             "PHRS": "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
             "WBTC": "0x8275c526d1bCEc59a31d673929d3cE8d108fF5c7",
